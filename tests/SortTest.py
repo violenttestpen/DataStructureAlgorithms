d6927e785b61f749745604d23462a1f64c45a6b7
Winston Ho <winstonho90@users.noreply.github.com>
2018-04-23 15:48:20 +0800

    + Added BubbleSort.py * Added BubbleSort profiling

--- tests/SortTest.py
+++ tests/SortTest.py
@@ -1,10 +1,19 @@
 import timeit
 
-setup = '''
+list_size = 1000
+number = 100
+
+setup = f'''
 import random
-array = list(range(1000))
+array = list(range({list_size}))
 random.shuffle(array)
 '''
 
-print(timeit.timeit('from Sort.InsertionSort import isort; isort(array)', setup=setup, number=100))
-print(timeit.timeit('from Sort.MergeSort import rmsort; rmsort(array)', setup=setup, number=100))
+print(f'BubbleSort for array of size {list_size} ({number} iterations)')
+print('Time taken: %.2fs' % timeit.timeit('from Sort.BubbleSort import bsort; bsort(array)', setup=setup, number=number))
+print(f'SelectionSort for array of size {list_size} ({number} iterations)')
+print('Time taken: %.2fs' % timeit.timeit('from Sort.SelectionSort import ssort; ssort(array)', setup=setup, number=number))
+print(f'InsertionSort for array of size {list_size} ({number} iterations)')
+print('Time taken: %.2fs' % timeit.timeit('from Sort.InsertionSort import isort; isort(array)', setup=setup, number=number))
+print(f'MergeSort for array of size {list_size} ({number} iterations)')
+print('Time taken: %.2fs' % timeit.timeit('from Sort.MergeSort import rmsort; rmsort(array)', setup=setup, number=number))
